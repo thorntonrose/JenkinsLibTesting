0ef540ec5d402a8447796f0a0a9bb721e600a75a
Thornton Rose <thornton.rose@bamtechmedia.com>
2018-08-06 14:29:27 -0400

    add ghost code for echo and logging

--- src/test/groovy/BaseTest.groovy
+++ src/test/groovy/BaseTest.groovy
@@ -1,11 +1,18 @@
 import org.junit.*
 import com.lesfurets.jenkins.unit.*
+import org.slf4j.*
 import static groovy.test.GroovyAssert.*
 
 class BaseTest extends BasePipelineTest {
+	// private static final log = LoggerFactory.getLogger(BaseTest)
+
 	@Before
 	void setUp() {
 		super.setUp()
+
+		// helper.registerAllowedMethod("echo", [ String ]) { arg ->
+		// 	log.debug "(echo) $arg"
+		// }
 
 		// load all steps from vars directory
 		new File("vars").eachFile { file ->
--- vars/getCommitHash.groovy
+++ vars/getCommitHash.groovy
@@ -2,5 +2,6 @@
  * getCommitHash returns the current git commit hash.
  */
 def call(Map opts = [:]) {
+	// echo "opts: $opts"
 	sh "git rev-parse HEAD"
 }
