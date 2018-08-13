0b61a35b32b6f12340d0c5b04c0df254d2b6a3dc
Thornton Rose <thornton.rose@bamtechmedia.com>
2018-08-13 14:14:22 -0400

    Add usage info on SonarQubeDeploy.

--- src/jobs/SonarQubeDeploy.groovy
+++ src/jobs/SonarQubeDeploy.groovy
@@ -1,5 +1,10 @@
 package jobs
 
+/**
+ * Usage:
+ *    new jobs.SonarQubeDeploy().call()
+ *    new jobs.SonarQubeDeploy().call <key>: <value>[, ...]
+ */
 def call(Map opts = [:]) {
 	try {
 		stage("Database") {
